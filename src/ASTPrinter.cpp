4a88d26995207df344a794339ef3d048c79bb1d6
give-me-fries <116779822+give-me-fries@users.noreply.github.com>
2025-01-09 16:59:14 -0800

    Remove debug statements

--- src/ASTPrinter.cpp
+++ src/ASTPrinter.cpp
@@ -24,25 +24,21 @@ void ASTPrinter::print( Expr* expr )
 
 void ASTPrinter::visit( Binary* expr )
 {
-    std::cout << "Binary\n";
     parenthesize( expr->op.getLexeme(),
                   { expr->left.get(), expr->right.get() } );
 }
 void ASTPrinter::visit( Grouping* expr )
 {
-    std::cout << "Grouping\n";
     parenthesize( "group", { expr->expr.get() } );
 }
 
 void ASTPrinter::visit( Literal* expr )
 {
-    std::cout << "Literal\n";
     m_tree += objectToString( expr->value );
 }
 
 void ASTPrinter::visit( Unary* expr )
 {
-    std::cout << "Unary\n";
     parenthesize( expr->op.getLexeme(), { expr->right.get() } );
 }
 
